63abc6b95d35552794b73d73034a76df1c278438
Braulio Henrique Marques Souto <braulio@disroot.org>
2023-06-18 12:42:51 -0300

    Adicionado configuração para Mensagens e adicionado redirecinamento após login e e-mail para terminal para Dev

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 
+from django.contrib.messages import constants
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,3 +123,18 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Config Messages
+MESSAGE_TAGS = {
+    constants.DEBUG: "alert-primary",
+    constants.ERROR: "alert-danger",
+    constants.SUCCESS: "alert-success",
+    constants.INFO: "alert-info",
+    constants.WARNING: "alert-warning",
+}
+
+# Redireciona para home URL após login (Padrão redireciona para /accounts/profile/)
+LOGIN_REDIRECT_URL = "/"
+
+# Para envio do email apenas pelo terminal para Desenvolvimento
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
