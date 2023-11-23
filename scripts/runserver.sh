923849916b1343907d815760950fbd38752bb0fa
Braulio Henrique Marques Souto <braulio@disroot.org>
2023-11-23 16:15:23 -0300

    Adicionado nivel de log para o seridor gunicorn

--- scripts/runserver.sh
+++ scripts/runserver.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #python manage.py runserver 0.0.0.0:8000
-gunicorn --bind 0.0.0.0:8000 core.wsgi --workers=4
+gunicorn --bind 0.0.0.0:8000 core.wsgi --workers=4 --log-level=info --log-file=/var/log/gunicorn/gunicorn.log
